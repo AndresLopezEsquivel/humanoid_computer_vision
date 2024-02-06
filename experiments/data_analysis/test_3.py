8acf6821a0a471b71ef2734bc05394a9df76eadc
Andrés López Esquivel <lopezesquivelandres1@gmail.com>
2024-02-06 15:37:32 -0600

    Add data to log

--- experiments/data_analysis/test_3.py
+++ experiments/data_analysis/test_3.py
@@ -5,23 +5,39 @@ import csv
 
 dt = 1/10
 
-with open('./logs/2024-01-30/test7.csv', newline = '') as csv_file:
-    data = [[float(row[0]),
-             float(row[1]),
-             float(row[2])]
+with open('./logs/2024-02-06/test2_feb6.csv', newline = '') as csv_file:
+    data = [[float(row[0]), # time
+             float(row[1]), # measured x-position
+             float(row[2]), # measured y-psotion
+             float(row[3]), # estimated x-position
+             float(row[4]), # estimated y-position
+             float(row[5]), # estimated x-velocity
+             float(row[6]) # estimated y-velocity
+            ]
              for row in csv.reader(csv_file)]
 
 start_time = data[0][0]
 
 data = [[row[0] - start_time,
          row[1],
-         row[2]]
+         row[2],
+         row[3],
+         row[4],
+         row[5],
+         row[6]]
          for row in data]
 
+l = 100 # lower
+h = len(data) # higher
+
 y = [row[2] for row in data]
-y = y[400:900]
+y = y[l:h]
+
+prev_y_vel = [row[6] for row in data]
+prev_y_vel = prev_y_vel[l:h]
+
 t = [row[0] for row in data]
-t = t[400:900]
+t = t[l:h]
 
 
 kf_y = []
@@ -32,7 +48,7 @@ R = np.identity(2) * 0.01
 kf = EKF(dt, Q, R)
 
 i = 0
-N = 150 # Number of samples
+N = 80 # Number of samples
 # N = len(y)
 while i < N:
     kf.predict()
@@ -54,6 +70,7 @@ y_position.plot(t, y[:N] + [None] * (len(y) - N), 'ro', label = '[m] (Mediciones
 y_position.plot(t, y, label = '[m] (Mediciones)')
 y_position.plot(t, kf_y, label = '[m] (Con KF)')
 y_position.plot(t, kf_vel_y, label = '[m/s] (Con KF)')
+y_position.plot(t, prev_y_vel, label = '[m/s] (prev KF)')
 y_position.set_xlabel(xlabel = 'Tiempo [s]')
 y_position.legend()
 y_position.grid(True)
