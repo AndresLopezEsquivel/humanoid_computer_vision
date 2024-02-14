32d7cf955b80afe8aa6de9e60beb12ce111ad292
Andrés López Esquivel <lopezesquivelandres1@gmail.com>
2024-02-14 09:50:07 -0600

    Modify MATLAB code to calculate undistorted pixels

--- experiments/get_undistorted_point/matlab/undistort_point.m
+++ experiments/get_undistorted_point/matlab/undistort_point.m
@@ -10,44 +10,28 @@ TangentialDist = [-0.00064995,  0.00094649];
 cameraParams = cameraParameters('K',K, ...
     'TangentialDistortion',TangentialDist, ...
     'RadialDistortion',RadialDist);
-% points = [0,0;
-%     640,0;
-%     1280,0;
-%     1280,360;
-%     1280,720;
-%     640,720;
-%     0,720;
-%     0,360;
-%     640,360];
-% 
-% undistortedPoints = undistortPoints(points,cameraParams);
 
-y = 0 : 720;
+y = 0 : 10 : 720;
 s = size(y);
 x = 0 * ones(1,s(2));
 pointsA = horzcat(x',y');
 
-x = 0 : 1280;
+x = 0 : 10 : 1280;
 s = size(x);
 y = 720 * ones(1,s(2));
 pointsB = horzcat(x',y');
 
-y = 720 : -1 : 0;
+y = 720 : -10 : 0;
 s = size(y);
 x = 1280 * ones(1,s(2));
 pointsC = horzcat(x',y');
 
-x = 1280 : -1 : 0;
-% x = 0 : 1280;
+x = 1280 : -10: 0;
 s = size(x);
 y = 0 * ones(1,s(2));
 pointsD = horzcat(x',y');
 
 points = vertcat(pointsA, pointsB, pointsC, pointsD);
-% plot(points(:,1), points(:,2), 'LineWidth', 2);
-% hold on;
-
-% points = [0,0;0,1280;1280,360;640,360];
 
 undistortedPoints = undistortPoints(points,cameraParams);
 
@@ -55,7 +39,21 @@ plot(undistortedPoints(:,1), ...
     undistortedPoints(:,2), ...
      'LineWidth',2);
 
-saveas(gcf,'points.png')
+hold on;
+
+plot(points(:,1), points(:,2), 'LineWidth', 2);
+
+legend('Sin distorsion', 'Con distorsion');
+title('Imagenes con y sin distorsion')
+xlabel('x [px]')
+ylabel('y [px]')
+
+ax = gca;
+ax.YDir = 'reverse';
+
+grid;
+
+% saveas(gcf,'points.png')
 % hold on;
 % plot(x',y','LineWidth',5)
 % imds = imageDatastore({'2023-02-12-192429.jpg'});
